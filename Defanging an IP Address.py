f415580ddc079725490081f7ad6f3f268f2cc7b1
OlegCherep <olegtcherpepanov@yandex.ru>
2022-01-29 16:13:43 +0300

    Убрал вызов str(i) для каждого элемента списка

---
+++ Defanging an IP Address.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        lst = list(address)
+        for i in range(len(lst)):
+            if lst[i] == ".":
+                lst[i] = "[.]"
+        return ''.join([str(i) for i in lst])
