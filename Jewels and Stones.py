607c57f2d52b3b9061e90fb26980894e1d5516d5
OlegCherep <olegtcherpepanov@yandex.ru>
2022-01-29 12:33:19 +0300

    Jewels and Stones
    Сравниваю значение драгоценных камней и обычных и нахожу общее.

---
+++ Jewels and Stones.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        k = 0
+        for i in range(len(stones)):
+            for j in range(len(jewels)):
+                if stones[i] == jewels[j]:
+                    k +=1 
+        return k
