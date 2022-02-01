91273691e58da92666e3ba2603e2464ff6542b26
OlegCherep <88234179+OlegCherep@users.noreply.github.com>
2022-02-01 20:02:57 +0300

    Update Smaller Numbers Than Current.py
    Изменил решение, уменьшив время выполнения до десятков миллисекунд.

--- Smaller Numbers Than Current.py
+++ Smaller Numbers Than Current.py
@@ -1,10 +1,15 @@
 class Solution:
-    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        lst = []
-        for i in range(len(nums)):
-            k = 0
-            for j in range(len(nums)):
-                if nums[i] > nums[j]:
-                    k += 1
-            lst.append(k)
-        return lst
+  def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+    m = {}
+    for num in nums:
+      m[num] = []
+    for i in range(len(nums)):
+      m[nums[i]].append(i)
+    lst = [0] * len(nums)
+    nums = sorted(list(set(nums)))
+    k = 0
+    for i in range(len(nums)):
+      for j in m[nums[i]]:
+        lst[j] = k
+      k += len(m[nums[i]])
+    return lst
