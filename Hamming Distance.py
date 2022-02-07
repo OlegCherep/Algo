e6aef65d195f5518584581e50ada3b951c31f13f
OlegCherep <88234179+OlegCherep@users.noreply.github.com>
2022-02-07 21:07:05 +0300

    Update Hamming Distance.py
    Заменил код, применив "побитовое исключающее или"

--- Hamming Distance.py
+++ Hamming Distance.py
@@ -1,17 +1,8 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        a = list(format(x, 'b'))
-        b = list(format(y, 'b'))
-        n1 = a[::-1]
-        n2 = b[::-1]
-        if len(n1) > len(n2):
-            for i in range(len(n1) - len(n2)):
-                n2.append('0')
-        else:
-            for i in range(len(n2) - len(n1)):
-                n1.append('0')
+        lst = list(format(x ^ y, 'b'))
         k = 0
-        for i in range(len(n1)):
-            if n1[i] != n2[i]:
+        for i in range(len(lst)):
+            if lst[i] == '1':
                 k += 1
         return k
