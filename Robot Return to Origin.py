227dadc823890dcf475ced28eb594a7db32af5ea
OlegCherep <88234179+OlegCherep@users.noreply.github.com>
2022-02-07 21:13:24 +0300

    Update Robot Return to Origin.py
    Изменил код, присвоив в словаре каждой букве счётчик и написал более грамотный вывод результата вычислений.

--- Robot Return to Origin.py
+++ Robot Return to Origin.py
@@ -1,8 +1,6 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        m = {"U": [],"D": [],"L": [],"R": []}
+        m = {"U": 0,"D": 0,"L": 0,"R": 0}
         for i in range(len(moves)):
-            m[moves[i]].append(i)
-        if len(m['U']) == len(m['D']) and (len(m['R']) == len(m['L'])):
-            return True
-        return False
+            m[moves[i]] += 1
+        return (m['U'] == m['D']) and (m['R'] == m['L'])
