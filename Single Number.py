cc38d84e97066bb6a486ee7ba884eb58b5596e2c
OlegCherep <88234179+OlegCherep@users.noreply.github.com>
2022-04-10 15:35:29 +0300

    Update Single Number.py
    Изменил код

--- Single Number.py
+++ Single Number.py
@@ -1,12 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        answ = 0
-        m = {}
-        for num in nums:
-            m[num] = 0
-        for num in nums:
-             m[num] += 1
-        for num in nums:
-            if m[num] == 1:
-                answ = num
-        return answ
+        ans = 0
+        for elem in nums:
+            ans = ans ^ elem
+        return ans
